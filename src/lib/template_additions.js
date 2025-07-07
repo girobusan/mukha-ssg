21d47dee4e801839f6c57ac744f051dd10d8bbc2
Girobusan <private>
2025-07-07 21:31:42 +0300

    + filter to_table

--- src/lib/template_additions.js
+++ src/lib/template_additions.js
@@ -1,7 +1,10 @@
-export function tableFilter(data) {
+export function tableFilter(data, columns) {
+  if (data.length === 0) {
+    return "";
+  }
   var r = `<table>
 <thead><tr>`;
-  const cols = Object.keys(data[0]);
+  const cols = columns || Object.keys(data[0]);
 
   cols.forEach((c) => (r += `<th>${c}</th>`));
 
