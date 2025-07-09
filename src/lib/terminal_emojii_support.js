139f590d8ebd36e7e93e47498fa44e2243282fc0
Girobusan <private>
2025-07-09 11:37:42 +0300

    + unicode in win

--- src/lib/terminal_emojii_support.js
+++ src/lib/terminal_emojii_support.js
@@ -1,20 +1,33 @@
+import process from "node:process";
+//
 var hasUnicode = function () {
-  // Recent Win32 platforms (>XP) CAN support unicode in the console but
-  // don't have to, and in non-english locales often use traditional local
-  // code pages. There's no way, short of windows system calls or execing
-  // the chcp command line program to figure this out. As such, we default
-  // this to false and encourage your users to override it via config if
-  // appropriate.
-  // if (os.type() == "Windows_NT") { return false }
-
   var isUTF8 = /UTF-?8$/i;
   var ctype = process.env.LC_ALL || process.env.LC_CTYPE || process.env.LANG;
   return isUTF8.test(ctype);
 };
 
+function thisWinHasUTF() {
+  // from
+  // @sindresorhus/is-unicode-supported
+  const { env } = process;
+  const { TERM, TERM_PROGRAM } = env;
+  return (
+    Boolean(env.WT_SESSION) || // Windows Terminal
+    Boolean(env.TERMINUS_SUBLIME) || // Terminus (<0.2.27)
+    env.ConEmuTask === "{cmd::Cmder}" || // ConEmu and cmder
+    TERM_PROGRAM === "Terminus-Sublime" ||
+    TERM_PROGRAM === "vscode" ||
+    TERM === "xterm-256color" ||
+    TERM === "alacritty" ||
+    TERM === "rxvt-unicode" ||
+    TERM === "rxvt-unicode-256color" ||
+    env.TERMINAL_EMULATOR === "JetBrains-JediTerm"
+  );
+}
+
 export function supportEmoji() {
   if (process.platform === "win32") {
-    return false;
+    return thisWinHasUTF();
   }
 
   return hasUnicode();
