d4dcbd2c7dc11a4dcf2e6f6fad7fbb92f525b585
Girobusan <private>
2025-07-11 19:31:06 +0300

    fix typo

--- src/backends/watch/fileops.js
+++ src/backends/watch/fileops.js
@@ -7,9 +7,9 @@ var log = getLogger("file ops");
 
 export function delFile(fp, in_dir) {
   let fname = path.basename(fp);
-  let newdir = path.join("deleted", fname);
   let trashparent = in_dir ? path.dirname(in_dir) : path.normalize(".");
   let trash = path.join(trashparent, "deleted");
+  let newdir = path.join(trash, fname);
   if (!fs.existsSync(trash)) {
     log.debug("Trash dir created at:", trash);
     fs.mkdirSync(trash);
