082a631cda4d60e3266fd84db8e63faaa8f56c25
Girobusan <private>
2025-07-14 20:32:27 +0300

    +close btn on search results

--- src/lib/search/client.js
+++ src/lib/search/client.js
@@ -21,25 +21,42 @@ import { Index } from "lunr";
     }
     const inpBB = inp.getBoundingClientRect();
     const Idx = Index.load(index);
-    const RW = document.createElement("div");
-    RW.classList.add("search_results");
-    RW.style.display = "none";
-    RW.style.position = "absolute";
-    RW.style.top = inpBB.top + inpBB.height + "px";
+    const results_window = document.createElement("div");
+    const res_content = document.createElement("div");
+    const closeBTN = document.createElement("button");
+    closeBTN.innerHTML = "×";
+    closeBTN.setAttribute(
+      "style",
+      "border:none;background:none;float:right;clear: both;font-size:1.5em;cursor:pointer;" +
+      "padding:0;margin:0;line-height:50%",
+    );
+    closeBTN.setAttribute("title", "close");
+    closeBTN.addEventListener(
+      "click",
+      () => (results_window.style.display = "none"),
+    );
+    results_window.appendChild(closeBTN);
+    results_window.appendChild(res_content);
+    results_window.classList.add("search_results");
+    results_window.style.display = "none";
+    results_window.style.position = "absolute";
+    results_window.style.top = inpBB.top + inpBB.height + "px";
     if (inpBB.right < 100) {
-      RW.style.right = inpBB.right + "px";
+      results_window.style.right = inpBB.right + "px";
+      results_window.style.marginLeft = "4rem";
     } else {
-      RW.style.left = inpBB.left + "px";
+      results_window.style.left = inpBB.left + "px";
+      results_window.style.marginRight = "4rem";
     }
-    RW.style.backgroundColor = "inherit";
-    RW.style.border = "1px solid currentColor";
-    RW.style.padding = "1rem";
-    document.body.appendChild(RW);
+    results_window.style.backgroundColor = "inherit";
+    results_window.style.border = "1px solid currentColor";
+    results_window.style.padding = "1rem";
+    document.body.appendChild(results_window);
 
     inp.classList.add("enabled-search-form");
     inp.addEventListener("input", () => {
       if (inp.value.length < 1) {
-        RW.style.display = "none";
+        results_window.style.display = "none";
         return;
       }
       let rawR = Idx.search(inp.value);
@@ -51,16 +68,16 @@ import { Index } from "lunr";
         };
       });
       // console.log(rawR);
-      RW.innerHTML = "";
+      res_content.innerHTML = "";
       if (R.length > 0) {
-        RW.style.display = "block";
+        results_window.style.display = "block";
         R.slice(0, 10).forEach(
           (e) =>
-            (RW.innerHTML += `<a href="${e.link}">${e.title}</a><div>${e.excerpt || ""}</div>`),
+            (res_content.innerHTML += `<a href="${e.link}">${e.title}</a><div class="search-excerpt">${e.excerpt || ""}</div>`),
         );
       } else {
-        RW.style.display = "none";
-        RW.innerHTML = "";
+        results_window.style.display = "none";
+        res_content.innerHTML = "";
       }
     });
   }
