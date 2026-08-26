ec14eb400ad7e84d3b073623b5600fa2aff47281
Girobusan <private>
2026-08-26 23:16:47 +0300

    + imporove components

--- src/lib/components/components.js
+++ src/lib/components/components.js
@@ -2,6 +2,7 @@ const preact = require("preact");
 const hooks = require("preact/hooks");
 const htm = require("htm/preact");
 const compat = require("preact/compat");
+import { renderToString } from "preact-render-to-string";
 
 import { getLogger } from "../logging";
 var log = getLogger("comps");
@@ -9,10 +10,10 @@ import { findRequires, normalizeName } from "./comp_util";
 //
 // dicts
 const internal = {
-  preact: { module: preact }, //module!!
-  "preact/hooks": { module: hooks },
-  "preact/compat": { module: compat },
-  "htm/preact": { module: htm },
+  preact: { exports: preact }, //module!!
+  "preact/hooks": { exports: hooks },
+  "preact/compat": { exports: compat },
+  "htm/preact": { exports: htm },
 };
 const loaded = {};
 const lookup = {};
@@ -29,18 +30,23 @@ function myRequire(n) {
   return M.module;
 }
 //
-export function findComponent(name) {
+export function findModuleForFn(fn_name) {
   // find component...
-  let mname = lookup[name];
-  if (!mname) {
-    console.error("Can not find entity", name);
+  let module_name = lookup[fn_name]; // name of the module
+  if (!module_name) {
+    console.error("Can not find any module with exported", fn_name);
     return null;
   }
-  return loaded[mname][name];
+  if (!loaded[module_name]) {
+    console.error("Can not find module", module_name, "with exported", fn_name);
+    return null;
+  }
+  return loaded[module_name].exports[fn_name];
 }
 //
-function runComponent(name, props) {
-  return findComponent(name)(props);
+export function createElement(fn_name, props) {
+  let fn = findModuleForFn(fn_name); // returns content of module.exports[fn_name]
+  return preact.h(fn, props);
 }
 //
 export function initComponents(flist) {
@@ -87,7 +93,7 @@ export function initComponents(flist) {
   log.info("Sort passes:", modulesTable.length - pass);
   // if something is left
   if (sortTable.length > 0) {
-    let unsatisfied = sortTable.reduce((a, e) => {
+    let not_found = sortTable.reduce((a, e) => {
       return a.concat(e.requires);
     }, []);
     log.warn("Some components are not loaded:");
@@ -98,7 +104,7 @@ export function initComponents(flist) {
         .sort()
         .join(", "),
     );
-    log.warn("Not found:", unsatisfied.sort().join(", "));
+    log.warn("Not found:", not_found.sort().join(", "));
     // if nothing is left, everything is ok (for now)
   } else {
     log.info("Components load order established.");
@@ -120,7 +126,7 @@ export function initComponents(flist) {
     //
     eval(mDict[n].src);
     loaded[n] = {
-      module: module.exports,
+      exports: module.exports,
       name: n,
       exported: new Set(Object.keys(module.exports)),
       requires: mDict[n].requires,
