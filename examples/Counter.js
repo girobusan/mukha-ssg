fd87138a56d266bdb6518c89c92f86863cff3d93
girobusan <girobusan@>
2026-09-11 13:56:33 +0300

    fix errors in example

--- examples/Counter.js
+++ examples/Counter.js
@@ -1,8 +1,8 @@
-const { html } = require("preact/htm");
+const { html } = require("htm/preact");
 const { useState } = require("preact/hooks");
 
 function Counter({ start = 12, step = 1 }) {
-  let { count, setCount } = useState(start);
+  let [ count, setCount ] = useState(start);
   return html`<div class="Counter">
     <div class="result" style="font-size:2em">${count}</div>
     <button onclick=${() => setCount(count - step)}>-</button>
