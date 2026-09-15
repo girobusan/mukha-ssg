3e22894b36660249c773687c5257d33a714592a8
Girobusan <private>
2026-09-15 22:25:27 +0300

    adding externals

--- searcher.config.js
+++ searcher.config.js
@@ -3,11 +3,19 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./src/lib/search/client.lib.js",
+
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "search_lib.js",
+
+    filename: "search-lib.js",
     library: {
       type: "commonjs2",
     },
+  },
+  externals: {
+    preact: "commonjs2 preact",
+    "preact/hooks": "commonjs2 preact/hooks",
+    "html/preact": "commonjs2 html/preact",
+    "mukha-system": "commonjs2 mukha-system",
   },
 };
